9889c8bf7617e17223491048548883f235462e65
Jatin Tyagi <tyagijatin6263@gmail.com>
2024-05-14 08:39:48 +0530

    import material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,107 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type Task = {
+  id: number;
+  name: string,
+  isDone: boolean;
 }
 
+const App = () => {
+  const [taskName, setTaskName] = useState<string>('')
+  const [tasks, setTask] = useState<Task[]>([])
+  useEffect(() => {
+    const tasks: string | null = localStorage.getItem('tasks');
+    if (tasks) {
+      setTask(JSON.parse(tasks));
+    }
+  }, []);
+
+  const onChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTaskName(event.target.value)
+  }
+
+  const onDoneTask = (event: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    const filteredList = tasks.filter((task) => {
+      return task.id === id;
+    });
+    filteredList[0].isDone = event.target.checked;
+    saveTasks([...tasks]);
+  }
+
+  const addList = (task: Task) => {
+    return <li key={task.id} className='task-item'>
+      <input type="checkbox" checked={task.isDone} onChange={(event) => onDoneTask(event, task.id)} />
+          <span>{task.name}</span>                      
+      {/* <button onClick={() => deleteTask(task.id)}>Delete</button> */}
+      <DeleteIcon style={{marginRight: "-150px"}} onClick={() => deleteTask(task.id)} /> 
+    </li>
+  }
+
+  const deleteTask = (id: number) => {
+    const filteredList = tasks.filter((task) => {
+      return task.id !== id;
+    });
+    saveTasks([...filteredList]);
+  }
+
+  const addTask = () => {
+
+    if (taskName.length < 5 && taskName.length > 0) {
+      return (
+        alert('Task length should be more than 5 character')
+      )
+    }
+    else if(taskName.length === 0){
+      return(
+        alert('Task Field is Empty. Enter the Task')
+      )
+    }
+    else if(taskName.length > 20){
+      return(
+        alert('Task should be less than 20 character')
+      )
+    }
+
+    const updatedTask = [
+      ...tasks,
+      {
+        id: Date.now(),
+        name: taskName,
+        isDone: false,
+      }
+    ]
+    saveTasks(updatedTask)
+    setTaskName('')
+
+  }
+
+  const saveTasks = (value: Task[]) => {
+    setTask(value);
+    localStorage.setItem('tasks', JSON.stringify(value));
+  }
+
+  return (
+    <div className='container'>
+      <h1>To-Do-Task-App</h1>
+
+      <div className='box'>
+        <input type='text' value={taskName} onChange={onChangeText} onKeyDown={(event: React.KeyboardEvent<HTMLInputElement>) => {
+          if (event.key === 'Enter') {
+            addTask();
+          }
+        }}></input>
+
+        <button onClick={addTask}>Add Task</button>
+      </div>
+
+      <div className='list-item'>
+        <ol className='list'>{tasks.map(addList)}</ol>
+      </div>
+
+    </div>
+  )
+
+}
 export default App;
